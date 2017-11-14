c641c888b8b125a73877e401f0cf3614129efd8b
GeorgijsT4501BD <32352474+GeorgijsT4501BD@users.noreply.github.com>
2017-11-14 20:04:31 +0200

    Add files via upload

---
+++ Calc.hs
@@ -0,0 +1,19 @@
+data Tree a = Node a (Tree a) (Tree a) | Leaf a deriving Show
+
+calc tree = calcTree $ toTree tree
+
+toTree tree = toTree' $ reverse tree
+
+toTree' (a:[]) = Leaf a
+toTree' (a:op:rest) = Node op (toTree' rest) (Leaf a)
+
+calcTree (Leaf a) = toNum a
+calcTree (Node op a b) = (operation op)  (calcTree a) (calcTree b)
+
+toNum num = read num :: Integer
+
+operation s
+  | s == "+" = (+)
+  | s == "-" = (-)
+  | s == "*" = (*)
+  | s == "/" = div
